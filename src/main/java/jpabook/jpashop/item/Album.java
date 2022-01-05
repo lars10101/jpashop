9d3a1c08884a1f0881b9f3370dfb65cf954e0cb6
mandooboy09 <mandooboy09@gmail.com>
2022-01-05 23:52:23 +0900

    엔티티 클래스 개발2

--- src/main/java/jpabook/jpashop/item/Album.java
+++ src/main/java/jpabook/jpashop/item/Album.java
@@ -1,4 +1,4 @@
-package jpabook.jpashop.domain.item;
+package jpabook.jpashop.item;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -7,11 +7,10 @@ import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
 @Entity
-@DiscriminatorValue("M")
+@DiscriminatorValue("A")
 @Getter @Setter
-public class Movie extends Item{
+public class Album extends Item{
 
-    private String director;
-    private String actor;
-
+    private String artist;
+    private String etc;
 }
--- src/main/java/jpabook/jpashop/item/Movie.java
+++ src/main/java/jpabook/jpashop/item/Movie.java
@@ -1,4 +1,4 @@
-package jpabook.jpashop.domain.item;
+package jpabook.jpashop.item;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -7,10 +7,11 @@ import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
 @Entity
-@DiscriminatorValue("B")
+@DiscriminatorValue("M")
 @Getter @Setter
-public class Book extends Item{
+public class Movie extends Item{
 
-    private String author;
-    private String isbn;
+    private String director;
+    private String actor;
+
 }
